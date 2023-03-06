9c31432204c2edbd26fa043379069f56897aca46
arapt <arapt@DESKTOP-70PT2F0>
2023-03-06 18:25:00 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,31 @@
+﻿class Animal<T>
+{
+    private T data;
+
+    public Animal(T data)
+    {
+        this.data = data;
+    }
+    public T getAnimal()
+    {
+        return data;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Animal<string> animalName = new Animal<string>("Turkey");
+        Animal<string> animalHabitat = new Animal<string>("Forest");
+        Animal<bool> endangered = new Animal<bool>(false);
+        Animal<bool> extinct = new Animal<bool>(false);
+        Animal<int> averageLifespan = new Animal<int>(5);
+
+        Console.WriteLine("This animal is a {0}.", animalName.getAnimal());
+        Console.WriteLine("It lives in a {0}.", animalHabitat.getAnimal());
+        Console.WriteLine("Is this animal endangered? {0}.", endangered.getAnimal() ? "Yes" : "No");
+        Console.WriteLine("Is this animal extinct? {0}.", extinct.getAnimal() ? "Yes" : "No");
+        Console.WriteLine("This animal lives for about {0} years.", averageLifespan.getAnimal());
+    }
+}
